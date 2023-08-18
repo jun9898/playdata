a2182e02ac0ed25d61fedbd10592150c8179e468
jun9898 <nice1998@gmail.com>
2023-08-18 21:56:34 +0900

    문제 풀어보기

---
+++ work/javawork/basicJava/src/assignment/date0818/TVFactory.java
@@ -0,0 +1,34 @@
+package assignment.date0818;
+
+public class TVFactory extends Factory implements IWorkingTogether{
+	
+	
+
+	public TVFactory(String name, int openHour, int closeHour) {
+		super(name, openHour, closeHour);
+	}
+
+	@Override
+	public int workTogether(IWorkingTogether partner) {
+		return 3*getWorkingTime();
+	}
+
+	@Override
+	int makeProducts(char skill) {
+		switch (skill) {
+			case 'A': {
+				return 8*getWorkingTime();
+			}
+			case 'B': {
+				return 5*getWorkingTime();
+			}
+			case 'C': {
+				return 3*getWorkingTime();
+			}
+			default: {
+				return 1;
+			}
+		}
+	}
+
+}
