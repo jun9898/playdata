a0518a5c461284b0e88dc127e22120f0ebea07f3
jun9898 <nice1998@gmail.com>
2023-08-31 17:52:45 +0900

    자바 스크립트 제어구문과 람다, 화살표 함수 학습함

--- work/webwork/basicjava/src/oop/intensification/Hex2Decimal.java
+++ work/webwork/basicjava/src/oop/intensification/Hex2Decimal.java
@@ -1,5 +1,32 @@
+package oop.intensification;
+
+import java.util.Scanner;
+
 public class Hex2Decimal {
-	
-	/*  이곳에 프로그램을 작성하십시오. */
-	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		boolean flag = true;
+		
+		while (flag == true) {
+			System.out.print("Enter the hex value ('quit' for exit) : ");
+			String hex = sc.next();
+			// String 문자열 비교는 equals
+			if (hex.equals("quit")) {
+				System.out.println("Bye !!!");
+				flag = false;
+			} else {
+				int result = 0;
+				try {
+					result = Integer.parseInt(hex,16);
+					System.out.println("hex input : " +hex);
+					System.out.println("decimal output : " +result);
+
+				} catch (Exception e) {
+					System.out.println("Invalid hex !!!");
+				}
+			}
+		}
+	}
 }
