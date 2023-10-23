c55f96603c08b6b65b3730c636e7d23cd38e2d7d
jun9898 <nice1998@gmail.com>
2023-10-23 17:50:24 +0900

    git과 React에 대해 학습함

---
+++ work/react/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>안녕 리엑트</h1>
+      <h1>나는 문어</h1>
+      <div>지금은 리엑트 연습중</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ work/react/react-bacis/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>리엑트 문법연습</h1>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
